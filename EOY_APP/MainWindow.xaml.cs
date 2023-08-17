1f72350481bab5a19f3d4a7132bec1aa1b25e271
AKuraksa <andrej.kuraksa@seznam.cz>
2023-08-17 23:19:30 +0200

    Kury- commit

--- EOY_APP/MainWindow.xaml.cs
+++ EOY_APP/MainWindow.xaml.cs
@@ -12,17 +12,73 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EOY_APP
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    
     public partial class MainWindow : Window
     {
+        bool firstClick = false;
+        bool maximalized = true;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape) 
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void SideBarMover_Click(object sender, RoutedEventArgs e)
+        {
+            if(!firstClick) 
+            {
+                Sidebar.MaxWidth = Sidebar.Width;
+                Sidebar.MinWidth = 30;
+                firstClick = true;
+            }
+            
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += (sender, e) => 
+            {   
+                if (maximalized)
+                {
+                    Sidebar.Width -= 10;
+                    if (Sidebar.Width == Sidebar.MinWidth)
+                    {
+                        maximalized = false;
+                        timer.Stop();
+                    }
+                    
+                }
+                else
+                {
+                    Sidebar.Width += 10;
+                    if (Sidebar.Width == Sidebar.MaxWidth)
+                    {
+                        maximalized = true;
+                        timer.Stop();
+                    }
+                    
+                }
+            };
+            timer.Start();
         }
     }
 }
