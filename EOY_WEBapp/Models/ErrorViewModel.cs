021346e4fd239f208c797bae13c2e1e396d01b3b
jan korizek <tomas.dohnal46@seznam.cz>
2023-09-05 20:23:16 +0200

    WEB 1phase

---
+++ EOY_WEBapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EOY_WEBapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
