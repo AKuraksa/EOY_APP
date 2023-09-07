36a905873486d2da15908c01a665b554cebbda3e
Tomáš DOHNAL <Tomas.DOHNAL@lotraco.cz>
2023-09-07 15:55:31 +0200

    zmena csHtml(nová věc)

--- EOY_WEBapp/Controllers/LoginController.cs
+++ EOY_WEBapp/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
 ﻿using EOY_WEBapp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using RestSharp;
+using System.Text.Json;
 
 namespace EOY_WEBapp.Controllers
 {
@@ -35,6 +37,41 @@ namespace EOY_WEBapp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-       
+        [HttpGet]
+        public IActionResult Index(LoginModel loginModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if(!ModelState.IsValid)
+                {
+                        var myClient = new RestClient();
+                        var request = new RestRequest();
+                        request.AddQueryParameter("username", loginModel.Username);
+                        request.AddQueryParameter("password", loginModel.Password);
+                        var response = myClient.Get(request);
+                        var content = response.Content;
+                        var user = JsonSerializer.Deserialize<List<LoginModel>>(content);
+                        if (user.Count() == 1)
+                        {
+                            return View(user);
+
+                        }
+                        else
+                        {
+                            return Error();
+                        }
+
+                    
+                }
+            }
+
+            // Pokud došlo k chybám validace, zobrazte formulář znovu s chybami
+            return View(loginModel);
+        }
+       private async Task GetUser(LoginModel loginModel)
+        {
+           
+        }
     }
 }
--- EOY_WEBapp/Models/LoginModel.cs
+++ EOY_WEBapp/Models/LoginModel.cs
@@ -8,14 +8,6 @@ namespace EOY_WEBapp.Models
        
             public string Username { get; set; }
             public string Password { get; set; }
-          
-        
 
-        async void GetLogin(string username, string password)
-        {
-            var myClient = new RestClient();
-            var request = new RestRequest();
-            request.AddQueryParameter("username", username);
-        }
     }
 }
