7d718542b3a354f7bb46d8dc3a6e8eee96ca35b2
jan korizek <tomas.dohnal46@seznam.cz>
2023-09-10 19:43:34 +0200

    oprava

---
+++ EOY_WEBapp/Controllers/WorkerController.cs
@@ -0,0 +1,16 @@
+﻿using EOY_WEBapp.Data;
+using EOY_WEBapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EOY_WEBapp.Controllers
+{
+    public class WorkerController : Controller
+    {
+        EOY_Functions _fce = new EOY_Functions();
+        public async Task<IActionResult> IndexWorker()
+        {
+            var listWorkers = await _fce.EOYrestResponse<WorkerModel>(EOY_Values.WORKERS_CONTROLLER, EOY_Values.GET);
+            return View(listWorkers);
+        }
+    }
+}
---
+++ EOY_WEBapp/Models/WorkerModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EOY_WEBapp.Models
+{
+    public class WorkerModel
+    {
+        [JsonPropertyName("id")]
+        public Guid ID { get; set; }
+        [JsonPropertyName("created")]
+        public DateTime Created { get; set; }
+        [JsonPropertyName("updated")]
+        public DateTime? Updated { get; set; }
+        [JsonPropertyName("workerFirstName")]
+        public string WorkerFirstName { get; set; }
+        [JsonPropertyName("workerLastName")]
+        public string WorkerLastName { get; set; }
+        [JsonPropertyName("authentificatorID")]
+        public string AuthentificatorID {get; set;}
+        [JsonPropertyName("loggedWorkplace")]
+        public string? LoggedWorkplace { get;set;}
+
+
+    }
+}
